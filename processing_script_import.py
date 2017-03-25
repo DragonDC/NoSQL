c88e67adb891d94789050c5703e818f5ee371f87
DragonDC <damianoss.c@gmail.com>
2017-03-25 16:50:37 +0100

    [task geo] - Add python script
    Python script which preprocess geojson to right format which could be imported to mongo.

---
+++ processing_script_import.py
@@ -0,0 +1,23 @@
+#1. Delete all blank lines from file
+file = "convertcsv.geojson"
+with open(file,"r") as fin:
+    lines=fin.readlines()
+with open(file,"w") as fin:  
+    [fin.write(line) for line in lines if line.strip() ]
+
+#2. Delete first three lines and the last one
+with open(file, 'r') as fin:
+    data = fin.read().splitlines(True)
+with open(file, 'w') as fin:
+    fin.writelines(data[3:-1])
+
+#3. Add at the beginning of file '['	
+f = open(file, "r")
+data = f.readlines()
+f.close()
+data.insert(0, '[\n')
+
+f = open(file, "w")
+data = "".join(data)
+f.write(data)
+f.close()	
